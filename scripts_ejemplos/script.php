6fe83a73ca92e282078f8dd92b47d39d3282fcfb
juanseMN <juansemontenegro7@gmail.com>
2019-03-15 12:02:40 -0500

    nuevo php

---
+++ scripts_ejemplos/script.php
@@ -0,0 +1,15 @@
+
+<center> programacion avanzada </center>
+<?php
+$nombre="peter";
+$apellido="watson";
+$edad=30;
+$salario=800;
+echo " <center> QUE DICE LA PEOPLE </center>";
+echo "Nombre:".$nombre."<br>";
+echo "Apellido:".$apellido;
+echo "<br>Edad:".$edad;
+echo "<br>Salario:".$salario;
+?>
+
+<center> fin del programa </center>
---
+++ scripts_ejemplos/script2.php
@@ -0,0 +1,17 @@
+<?php
+// Esta es mi primera calculadora
+$num1=10;
+$num2=5;
+
+//$suma=$num1+$num2;
+$resta=$num1-$num2;
+$mult=$num1*$num2;
+$div=$num1/$num2;
+
+echo "<center>";
+echo "la suma es" .($num1-$num2);
+echo "<br>la resta es" .$resta;
+echo "<br>la multiplicacion es" .$mult;
+echo "<br>la division es" .$div;
+echo "</center>";
+ ?>
